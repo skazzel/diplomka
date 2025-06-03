a0ad07e4e6287b32376e1c2e018fc9d48198c5a3
skazzel <skazzel@seznam.cz>
2025-06-03 18:44:57 +0200

    unwanted trash

--- javafx-frontend/src/main/java/cz/vutbr/fit/hospitu/frontend/view/IViewSwitcher.java
+++
@@ -1,9 +0,0 @@
-package cz.vutbr.fit.hospitu.frontend.view;
-
-import java.io.IOException;
-
-@FunctionalInterface
-public interface IViewSwitcher
-{
-    void switchView(String viewName) throws IOException;
-}
